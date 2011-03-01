90332203a43f5edb6bf9953d566c7e385ed85016
Alin <alin@zen.(none)>
2011-03-02 00:24:15 +0200

    - Update skin layout.

--- ana/gheorghe/browser/viewlets/interfaces.py
+++ ana/gheorghe/browser/viewlets/interfaces.py
@@ -6,6 +6,10 @@ class IPortalHeader(IViewletManager):
     """A viewlet manager that sits in the portal header
     """
 
+class IPortalSections(IViewletManager):
+    """A viewlet manager that sits in left side of the content
+    """
+
 class IPortalFooter(IViewletManager):
     """A viewlet manager that sits in the portal footer
     """
